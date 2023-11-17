23f032c371e05d530906db217a8ad1136f478d2a
Juan Alejandro Espinosa <65057841+alejoec03@users.noreply.github.com>
2023-11-17 18:53:34 -0500

    Update inmueble.php

--- inmueble/inmueble.php
+++ inmueble/inmueble.php
@@ -8,7 +8,7 @@ include "../includes/header.php";
 
             <div class="row">
                 
-                <div class="col-lg-12 m-2 p-3" style="background-color: #fff; border: solid #e3e4e5 1px; border-radius: 8px;">
+                <div class="col-lg-12 p-3" style="background-color: #fff; border: solid #e3e4e5 1px; border-radius: 8px;">
 
                     <h2>
                         Añadir Inmueble
@@ -16,62 +16,114 @@ include "../includes/header.php";
 
                     <hr>
 
-                    <label class="form-label">Tipo</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option value= "0"selected>Seleccione uno</option>
-                        <option value="Casa">Casa</option>
-                        <option value="Apartamento">Apartamento</option>
-                        <option value="Oficina">Oficina</option>
-                        <option value="Apartaestudio">Apartaestudio</option>
-                        <option value="Bodega">Bodega</option>
-                        <option value="Lote">Lote</option>
-                        <option value="Complejo">Complejo Industrial</option>
-                        <option value="Almacen">Almacen</option>
-                        <option value="Edificio">Edificio</option>
-                    </select>
+                    <div class="row">
+                        
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Tipo</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option value= "0"selected>Seleccione uno</option>
+                                <option value="Casa">Casa</option>
+                                <option value="Apartamento">Apartamento</option>
+                                <option value="Oficina">Oficina</option>
+                                <option value="Apartaestudio">Apartaestudio</option>
+                                <option value="Bodega">Bodega</option>
+                                <option value="Lote">Lote</option>
+                                <option value="Complejo">Complejo Industrial</option>
+                                <option value="Almacen">Almacen</option>
+                                <option value="Edificio">Edificio</option>
+                            </select>
+                        </div>
 
-                    <label class="form-label">Area</label>
-                    <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Area" required>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Area</label>
+                            <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Area" required>
+                        </div>
 
-                    <label class="form-label">Direccion</label>
-                    <input type="text" name="" class="form-control mb-2" placeholder="Ingrese la Direccion" required>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Direccion</label>
+                            <input type="text" name="" class="form-control mb-2" placeholder="Ingrese la Direccion" required>
+                        </div>
 
-                    <label class="form-label">Estado</label>
+                    </div>
+
+                     <div class="row">
+                        
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Estado</label>
                     <select class="form-select" aria-label="Default select example">
                         <option value= "0"selected>Seleccione uno</option>
                         <option value="Disponible">Disponible</option>
                         <option value="No Disponible">No Disponible</option>
                     </select>
+                        </div>
 
-                    <label class="form-label">Estrato</label>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Estrato</label>
                     <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Estrato" required>
+                        </div>
 
-                    <label class="form-label">Precio</label>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Precio</label>
                     <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Precio" required>
+                        </div>
 
-                    <label class="form-label">Numero de Habitaciones</label>
+                    </div>
+
+                     <div class="row">
+                        
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Numero de Habitaciones</label>
                     <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Numero" required>
+                        </div>
 
-                    <label class="form-label">Numero de Baños</label>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Numero de Baños</label>
                     <input type="number" name="" class="form-control mb-2" placeholder="Ingrese el Numero" required>
+                        </div>
 
-                    <label class="form-label">Cedula del Dueño</label>
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Cedula del Dueño</label>
                     <select class="form-select" aria-label="Default select example">
                         <option value= "0"selected>Seleccione una</option>
                         <option value="1001">1001</option>
                         <option value="1002">1002</option>
                         <option value="1003">1003</option>
                     </select>
+                        </div>
 
-                    <label class="form-label">Cedula del Ocupante*</label>
+                    </div>
+
+                     <div class="row">
+                        
+
+                        <div class="form-group col-md-4">
+                            <label class="form-label">Cedula del Ocupante*</label>
                     <select class="form-select" aria-label="Default select example">
                         <option value= "0"selected>Seleccione una</option>
                         <option value="1001">1001</option>
                         <option value="1002">1002</option>
                         <option value="1003">1003</option>
                     </select>
+                        </div>
 
-                    <button type="button" class="btn btn-primary form-control mt-2">Añadir</button>
+                        <div class="form-group col-md-4">
+                            <label for=""></label>
+                            <button type="button" class="btn btn-primary form-control mt-2">Añadir</button>
+                        </div>
+
+                    </div>
+
+                    
+
+                    
+
+      
+
+                    
+
+                    
+
+                    
                 
                 </div>
 
